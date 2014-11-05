9ffd3cd8b545729226141ffbc4aa4ac86a1a3fe7
jslopeza <jasonlopeza95@gmail.com>
2014-11-05 13:51:22 -0500

    Added SASS conversion to CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,19 @@
 var gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')();
 
+gulp.task('sass', function(){
+	return gulp.src([
+			'app/styles/*.scss',
+			'public/styles/sass/*.scss'
+		])
+		.pipe(plugins.plumber())
+		.pipe(plugins.rubySass({
+			style : 'expanded'
+		}))
+		.pipe(gulp.dest('./tmp'))
+		gulp.start('styles');
+});
+
+gulp.takk('styles', function(){
+
+});

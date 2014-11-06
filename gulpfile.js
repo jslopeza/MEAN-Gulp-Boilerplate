8941f9686f4c6f7fe657e3a6b13b89f9dc86067c
jslopeza <jasonlopeza95@gmail.com>
2014-11-05 23:01:44 -0500

    Fixed browser-sync and nodemon loading issue

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')(),
     browserSync = require('browser-sync'),
-    BROWSER_SYNC_RELOAD_DELAY = 2000;
+    BROWSER_SYNC_RELOAD_DELAY = 5000;
 
+// Compile SASS to CSS and minify CSS
 gulp.task('styles', function() {
     return gulp.src('app/styles/*.scss')
         .pipe(plugins.plumber())
@@ -22,28 +23,32 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./public/styles/'));
 });
 
-gulp.task('clean', function() {
-    return gulp.src('./tmp')
-        .pipe(plugins.clean({
-            read: false
-        }));
+// Minify JS
+gulp.task('scripts', function(){
+	return gulp.src('./public/js/**/*.js')
+		.pipe(gulp.dest('./tmp/js'))
+		.pipe(plugins.uglify())
+		.pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        proxy: 'http://localhost:3000',
-        port: 4000
-    });
+// Browser-Sync
+gulp.task('browser-sync',['nodemon'], function() {
+    setTimeout(function(){
+    	browserSync.init(null, {
+	        proxy: 'http://localhost:3000',
+	        port: 4000
+	    });
+    },BROWSER_SYNC_RELOAD_DELAY);
 });
 
-gulp.task('nodemon', ['browser-sync'],function(cb) {
+// Nodemon for running the app server
+gulp.task('nodemon', function(cb) {
 	var called = false;
-    plugins.nodemon({
-        script: 'server.js',
-        ext: 'html js',
-    })
+    plugins.nodemon({script: 'server.js'})
         .on('start', function() {
-            if(!called) {cb();}
+            if(!called) {
+            	cb();
+            }
             called = true;
         })
         .on('restart', function() {
@@ -53,15 +58,42 @@ gulp.task('nodemon', ['browser-sync'],function(cb) {
         });
 });
 
-gulp.task('serve',['nodemon'],function() {
+// Minify Images
+gulp.task('images', function(){
+	return gulp.src('./public/img/*')
+		.pipe(gulp.dest('./tmp/img'))
+		.pipe(plugins.imagemin())
+		.pipe(gulp.dest('./public/img/'));
+});
+
+// Local webserver watching for changes
+gulp.task('serve',['styles','browser-sync'],function() {
     return gulp.watch([
         'app/styles/*.scss',
         'app/**/*.js',
         'public/js/*.js',
         'public/styles/*.css',
-        'public/styles/**/*.scss',
         'public/views/*.html'
-    ]);
+    ])
+    .on('change',function(){
+    	'styles';
+    	browserSync.reload();
+    });
 });
 
-gulp.task('default', ['styles', 'clean']);
+// Remove Temporary folders
+gulp.task('clean', function() {
+    return gulp.src(['./tmp','.bower-cache','.bower-registry','.bower-tmp','.sass-cache'])
+        .pipe(plugins.clean({
+            read: false
+        }));
+});
+
+// Build the final version
+gulp.task('build', ['styles', 'scripts','images','clean']);
+
+// Instructions
+gulp.task('default',function(){
+	console.log("Run `gulp serve` to run a local webserver with browser-sync");
+	console.log("Run `gulp build` for a production ready code");
+});
